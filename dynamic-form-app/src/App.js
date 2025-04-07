46fe00ccbf5ef1a2d3f51d667f72185082798bbc
Ojas Joshi <146631578+Joshiojas185@users.noreply.github.com>
2025-04-07 15:05:08 +0530

    Update App.js

--- dynamic-form-app/src/App.js
+++ dynamic-form-app/src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DynamicForm from './components/DynamicForm';
+import DynamicFormGenerator from './components/DynamicFormGenerator';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/form/:tableName" element={<DynamicForm />} />
+        <Route path="/forms" element={<DynamicFormGenerator />} />
       </Routes>
     </Router>
   );
